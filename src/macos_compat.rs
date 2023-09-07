a57d0acda40fb2524894c10f28f2ca3a55851585
Felix S. Klock II <pnkfelix@pnkfx.org>
2023-09-07 10:54:59 -0400

    Added AArch64 support atop Mac M1.
    Also: refactored code to put asm-based implmentation into its own submodule.
    
    Also: refactored the assembly sequences to leverage fact that code is same for setjmp and sigsetjmp.
    
    Also: includes bug fix for sigsetjmp which was missing this forget that I had remembered for setjmp at some point.

--- src/macos_compat.rs
+++ src/macos_compat.rs
@@ -1,10 +1,17 @@
 use std::marker::PhantomData;
 
+#[cfg(target_arch = "aarch64")]
+const JMP_BUF_SIZE: usize = 48;
+#[cfg(target_arch = "x86_64")]
+const JMP_BUF_SIZE: usize = 37;
+
+const SIG_JMP_BUF_SIZE: usize = JMP_BUF_SIZE + 1;
+
 /// `JmpBufFields` are the accessible fields when viewed via a JmpBuf pointer.
 /// But also: You shouldn't be poking at these!
 #[repr(C)]
 pub struct JmpBufFields {
-    _buf: [u32; 37],
+    _buf: [u32; JMP_BUF_SIZE],
     _neither_send_nor_sync: PhantomData<*const u8>,
 }
 
@@ -12,7 +19,7 @@ pub struct JmpBufFields {
 /// But also: You shouldn't be poking at these!
 #[repr(C)]
 pub struct SigJmpBufFields {
-    _buf: [u32; 38],
+    _buf: [u32; SIG_JMP_BUF_SIZE],
     _neither_send_nor_sync: PhantomData<*const u8>,
 }
 
